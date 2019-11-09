6dba13777827907a0c15eaa4e6d3e96f0e23c1c4
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-09 15:37:32 +0000

    to add_to_stock

--- pet_shop.rb
+++ pet_shop.rb
@@ -25,10 +25,32 @@ end
 
 def pets_by_breed(shop,breed)
   pets = []
-    for elements in shop[:pets]
-      if elements[:breed] == breed
-        pets << elements[:pets]
+    for animals in shop[:pets]
+      if animals[:breed] == breed
+        pets << animals[:pets]
       end
     end
     return pets
 end
+
+def find_pet_by_name(shop, name)
+  for animals in shop[:pets]
+    if animals[:name] == name
+      pet = animals
+    end
+  end
+  return pet
+end
+
+def remove_pet_by_name(shop,name)
+  for animals in shop[:pets]
+    if animals[:name] == name
+      animals.shift
+    end
+  end
+end
+
+# def add_pet_to_stock(shop,new_pet)
+#   new_pet = {@new_pet}
+#   p new_pet
+# end
